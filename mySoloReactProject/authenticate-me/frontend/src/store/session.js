35999dd3b3d0e369d9aa97bec5db54c2b2439c0b
Gustavo <g.ebenezer.miranda@gmail.com>
2021-06-18 17:40:52 -0700

    FINISH PHASE 3

--- mySoloReactProject/authenticate-me/frontend/src/store/session.js
+++ mySoloReactProject/authenticate-me/frontend/src/store/session.js
@@ -32,6 +32,16 @@ export const login = (user) => async (dispatch) => {
 
 
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+
+
 export const signup = (user) => async (dispatch) => {
   const { username, email, password } = user;
   const response = await csrfFetch("/api/users", {
