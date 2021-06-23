69c0c23e4103beeb4ee768178508e98253140d8c
Gustavo <g.ebenezer.miranda@gmail.com>
2021-06-23 00:50:44 -0700

    BG IMAGE AND GRID LAYOUT COMPLETE

--- mySoloReactProject/authenticate-me/frontend/src/components/Navigation/index.js
+++ mySoloReactProject/authenticate-me/frontend/src/components/Navigation/index.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+// import romeImage from '../../../public/images/rome-6207755_1920.jpg';
+// const romeImage = require('/home/eben/appacademyfolder/week15/day4/WEEK-15-DAY-4-MY-SOLO-REACT-PROJECT/mySoloReactProject/authenticate-me/frontend/public/images/rome-6207755_1920.jpg');
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -21,13 +23,25 @@ function Navigation({ isLoaded }){
     );
   }
 
+  // const romeImageLink = 'https://cdn.pixabay.com/photo/2021/04/25/23/17/rome-6207755_960_720.jpg';
+
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    <>
+    <div className="homeDivContainer">
+      <img className="homeBackground__romeImage" src="../../images/rome-6207755_1920.jpg"></img>
+      <div className="outerGridDiv">
+      <div id="ulNavLinkWrapperDiv">
+        <ul id="loggedOutNavLinks">
+          <li>
+            <NavLink exact to="/">Home</NavLink>
+            {isLoaded && sessionLinks}
+          </li>
+        </ul>
+        </div>
+      </div>
+    </div>
+    <div className="test"></div>
+    </>
   );
 }
 
